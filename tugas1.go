3d60f0507dc791639fcc8b28eabde213a6f3332f
TehPuccupHarum <tehpuccupharum@gmail.com>
2024-03-26 18:36:39 +0700

    membuat data utama

---
+++ tugas1.go
@@ -0,0 +1,14 @@
+/* tugas 1 yaitu dibawah ini
+1. Hitung skor rata-rata untuk setiap tim, menggunakan data uji di bawah ini.
+2. Bandingkan skor rata-rata tim untuk menentukan pemenang kompetisi, dan cetak ke konsol. Jangan lupa bahwa bisa ada hasil seri, jadi uji juga untuk itu (seri berarti mereka memiliki skor rata-rata yang sama).
+3. Bonus 1: Sertakan persyaratan untuk skor minimum 100. Dengan aturan ini, sebuah tim hanya menang jika memiliki skor lebih tinggi dari tim lain, dan pada saat yang sama skor minimal 100 poin. Petunjuk: Gunakan operator logika untuk menguji skor minimum, serta beberapa blok else-if.
+4. Bonus 2: Skor minimum juga berlaku untuk seri! Jadi seri hanya terjadi ketika kedua tim memiliki skor yang sama dan keduanya memiliki skor lebih besar atau sama dengan 100 poin. Jika tidak, tidak ada tim yang memenangkan trofi.
+*/
+
+package main
+
+func main() {
+	// Data Utama
+	lumbaLumba := []int{96, 108, 89}
+	koala := []int{88, 91, 110}
+}
